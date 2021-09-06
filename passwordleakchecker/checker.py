5c9ac3387674bb28e662036edeeadb6883fa9a6f
RSNAYA <90208770+RSNAYA@users.noreply.github.com>
2021-09-06 12:29:45 -0400

    Create checker.py

---
+++ passwordleakchecker/checker.py
@@ -0,0 +1,54 @@
+try:
+    import requests
+    import hashlib
+except ModuleNotFoundError:
+    print('Modules not found.')
+    exit()
+
+
+def request_api_data(hash_char):
+    # fetches data from the url and stores it in res
+    res = requests.get('https://api.pwnedpasswords.com/range/'+hash_char)
+    if res.status_code != 200:
+        raise RuntimeError(
+            f'error fetching: {res.status_code}, check the API and try again')
+    return res
+
+
+def password_leak_count(hashes, hash_to_check):
+    # sepetares the hash and count
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:  # h stores the hash and count stores the number of times it's been cracked
+        if h == hash_to_check:
+            return count
+    return 0
+
+
+def pwned_api_check(password):
+    # encodes the password in sha1 hash
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_chars, rest = sha1password[:5], sha1password[5:]
+    # passes the first five chars in the funct and receives matching responses
+    response = request_api_data(first5_chars)
+    return password_leak_count(response, rest)
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(
+                f'{password} was found to be leaked {count} times. Change your password!')
+        else:
+            print(f'{password} was not found to be leaked. You are safe!')
+    return 'done!'
+
+
+if __name__ == '__main__':
+    print("password checker")
+    usr_inp = input('Enter password to be checked for leaks: ')
+    splited = usr_inp.split()
+    main(splited)
+    print("")
+    input('Press ENTER to exit') 
+
